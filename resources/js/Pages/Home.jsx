e2ddd4ed83a68512dd9ddee5b3c837dfcc39a252
rstubryan <restubumirr@gmail.com>
2024-11-17 22:34:46 +0700

    feat(task): add create task functionality

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,53 @@
-import { Head } from "@inertiajs/react";
+import { useState } from "react";
+import { Head, useForm } from "@inertiajs/react";
 import Header from "@/Components/molecules/Header";
-import { PomoHeading } from "@/Components/atoms/Typography";
+import { Paragraph, PomoHeading } from "@/Components/atoms/Typography";
 import { Container } from "@/Components/templates/Container.jsx";
 import { Button } from "@/Components/ui/button.jsx";
-import { CircleStop, Play } from "lucide-react";
+import { CircleHelp, CircleStop, Play, Plus } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Input } from "@/Components/ui/input.jsx";
+import { Label } from "@/Components/ui/label.jsx";
 
 export default function Home() {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        task_name: "",
+        task_pomo: null,
+    });
+
+    const [calculatedTime, setCalculatedTime] = useState("");
+
+    function handlePomoChange(e) {
+        const pomoValue = e.target.value;
+        setData("task_pomo", pomoValue);
+
+        const totalMinutes = pomoValue * 25;
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+        setCalculatedTime(`${hours} hours and ${minutes} minutes`);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        post("/task", {
+            preserveScroll: true,
+            onSuccess: () => reset(),
+        });
+    }
+
     return (
         <>
             <Head title="Workpace" />
@@ -30,11 +72,124 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="justify-center items-center min-h-screen bg-[#242424] p-10 lg:text-start text-center">
-                        <PomoHeading
-                            className={`text-secondary dark:text-primary`}
-                        >
-                            Task
-                        </PomoHeading>
+                        <div className="lg:flex justify-between items-center ">
+                            <PomoHeading
+                                className={`text-secondary dark:text-primary`}
+                            >
+                                Task
+                            </PomoHeading>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button
+                                        className={`lg:w-max w-full lg:mt-0 mt-5 lg:p-10 p-0`}
+                                    >
+                                        <Plus />
+                                    </Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Create Task</DialogTitle>
+                                        <DialogDescription>
+                                            <form
+                                                className={`mt-3`}
+                                                onSubmit={handleSubmit}
+                                            >
+                                                <Label htmlFor={`task_name`}>
+                                                    Task Name
+                                                </Label>
+                                                <Input
+                                                    type={`text`}
+                                                    id={`task_name`}
+                                                    name={`task_name`}
+                                                    placeholder={`Task name`}
+                                                    className={`w-full my-2`}
+                                                    value={data.task_name}
+                                                    onChange={(e) =>
+                                                        setData(
+                                                            "task_name",
+                                                            e.target.value,
+                                                        )
+                                                    }
+                                                    required
+                                                />
+                                                {errors.task_name && (
+                                                    <div>
+                                                        {errors.task_name}
+                                                    </div>
+                                                )}
+                                                <TooltipProvider>
+                                                    <Tooltip>
+                                                        <TooltipTrigger>
+                                                            <Label
+                                                                htmlFor={`task_pomo`}
+                                                                className={`flex gap-2 items-center`}
+                                                            >
+                                                                Pomo's Time
+                                                                <CircleHelp
+                                                                    size={14}
+                                                                />{" "}
+                                                                <span className="text-primary/30 text-xs">
+                                                                    (Hover for
+                                                                    more info)
+                                                                </span>
+                                                            </Label>
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            <p>
+                                                                1 Pomo = 25
+                                                                minutes
+                                                            </p>
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                </TooltipProvider>
+                                                <Input
+                                                    type={`number`}
+                                                    id={`task_pomo`}
+                                                    name={`task_pomo`}
+                                                    placeholder={`Pomo's time`}
+                                                    className={`w-full my-2`}
+                                                    value={data.task_pomo}
+                                                    onChange={handlePomoChange}
+                                                    onKeyDown={(e) => {
+                                                        if (
+                                                            e.key === "." ||
+                                                            e.key === ","
+                                                        ) {
+                                                            e.preventDefault();
+                                                        }
+                                                    }}
+                                                    step={`1`}
+                                                    required
+                                                />
+                                                {calculatedTime && (
+                                                    <Label
+                                                        htmlFor={`task_pomo`}
+                                                        className={`text-primary/50 text-sm py-2`}
+                                                    >
+                                                        You will spend{" "}
+                                                        <span className="font-semibold">
+                                                            {calculatedTime}
+                                                        </span>
+                                                    </Label>
+                                                )}
+                                                {errors.task_pomo && (
+                                                    <div>
+                                                        {errors.task_pomo}
+                                                    </div>
+                                                )}
+                                                <Button
+                                                    type={`submit`}
+                                                    className={`w-full mt-2`}
+                                                    disabled={processing}
+                                                >
+                                                    Submit
+                                                </Button>
+                                            </form>
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
                     </div>
                 </div>
             </Container>
