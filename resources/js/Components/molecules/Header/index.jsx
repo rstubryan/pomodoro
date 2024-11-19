e5798b71a88d05969fdb311583d043113997817e
restubryanr <restubumirr@gmail.com>
2024-11-19 11:05:33 +0700

    feat(header): update header component with user auth and dropdown menu

--- resources/js/Components/molecules/Header/index.jsx
+++ resources/js/Components/molecules/Header/index.jsx
@@ -1,28 +1,36 @@
 import { Container } from "@/Components/templates/Container.jsx";
 import { Heading } from "@/Components/atoms/Typography/index.jsx";
 import { ThemeProvider } from "@/Components/theme-provider.jsx";
-import { Link, usePage } from "@inertiajs/react";
+import { Link } from "@inertiajs/react";
 import { ModeToggle } from "@/Components/mode-toggle.jsx";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import { useAuth } from "@/hooks/user-auth";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu";
 
 export default function Header() {
+    const userAuth = useAuth();
+
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const url = usePage().url;
     const navList = [
         {
-            title: "Dashboard",
-            href: "/dashboard",
-            active: url === "/dashboard" && true,
-            auth: true,
+            title: userAuth ? `Hi, ${userAuth.name}` : "Login",
+            href: userAuth ? "/dashboard" : "/login",
         },
     ];
 
     return (
         <div className="border-b shadow-sm">
-            <Container className="lg:flex lg:justify-between items-center my-5">
-                <div className="flex justify-between items-center">
+            <Container className="my-5 items-center lg:flex lg:justify-between">
+                <div className="flex items-center justify-between">
                     <Heading>Workpace</Heading>
                     <div className="lg:hidden">
                         {isMenuOpen ? (
@@ -34,19 +42,43 @@ export default function Header() {
                 </div>
                 <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                     <nav
-                        className={`lg:flex ${isMenuOpen ? "grid" : "hidden"} lg:items-center lg:gap-5 mt-5 lg:mt-0`}
+                        className={`lg:flex ${isMenuOpen ? "grid" : "hidden"} mt-5 lg:mt-0 lg:items-center lg:gap-5`}
                     >
-                        <ul className="lg:flex grid gap-5 items-center">
+                        <ul className="grid items-center gap-5 lg:flex">
                             {navList.map((navItem, index) => (
                                 <li key={index}>
-                                    <Link
-                                        href={navItem.href}
-                                        className={
-                                            navItem.active ? "font-bold" : ""
-                                        }
-                                    >
-                                        {navItem.title}
-                                    </Link>
+                                    {userAuth ? (
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger>
+                                                {navItem.title}
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent>
+                                                <DropdownMenuLabel>
+                                                    <Link href={navItem.href}>
+                                                        Dashboard
+                                                    </Link>
+                                                </DropdownMenuLabel>
+                                                <DropdownMenuSeparator />
+                                                <DropdownMenuItem>
+                                                    <Link href="/profile">
+                                                        Profile
+                                                    </Link>
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    <Link
+                                                        method="post"
+                                                        href={route("logout")}
+                                                    >
+                                                        Log Out
+                                                    </Link>
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                    ) : (
+                                        <Link href={navItem.href}>
+                                            {navItem.title}
+                                        </Link>
+                                    )}
                                 </li>
                             ))}
                             <ModeToggle />
